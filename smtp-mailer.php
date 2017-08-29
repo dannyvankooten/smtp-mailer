e76f59ddb653607bf4e797043c25b08636d18a5a
Danny <dannyvankooten@gmail.com>
2017-08-29 14:31:56 +0200

    be smart about setting SMTPAuth so plugin works with mailcatcher

--- smtp-mailer.php
+++ smtp-mailer.php
@@ -11,50 +11,42 @@ Private: True
 namespace SMTP_Mailer;
 
 if( ! defined( 'ABSPATH' ) ) {
-	exit;
+    exit;
 }
 
 class Plugin {
 
-	/**
-	 * @var array Necessary configuration constants, these should be defined in your /wp-config.php file
-	 */
-	private $constants = array(
-		'SMTP_HOST',
-		'SMTP_PORT',
-		'SMTP_USER',
-		'SMTP_PASSWORD'
-	);
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        add_action( 'phpmailer_init', array( $this, 'setup_phpmailer' ) );
+    }
 
-	/**
-	 * Constructor
-	 */
-	public function __construct() {
-		add_action( 'phpmailer_init', array( $this, 'setup_phpmailer' ) );
-	}
+    /**
+     * Alter the PHPMailer object
+     *
+     * @param $phpmailer
+     */
+    public function setup_phpmailer( \PHPMailer $phpmailer ) {
 
-	/**
-	 * Alter the PHPMailer object
-	 *
-	 * @param $phpmailer
-	 */
-	public function setup_phpmailer( $phpmailer ) {
+        // make sure all configuration constants are given
+        if( ! defined( 'SMTP_HOST' ) || ! defined( 'SMTP_PORT' ) ) {
+            return;
+        }
 
-		// make sure all configuration constants are given
-		foreach( $this->constants as $constant ) {
-			if( ! defined( $constant ) || '' === constant( $constant ) ) {
-				return;
-			}
-		}
+        $phpmailer->Mailer = 'smtp';
+        $phpmailer->Host = SMTP_HOST;
+        $phpmailer->Port = SMTP_PORT;
 
-		$phpmailer->Mailer = 'smtp';
-		$phpmailer->SMTPSecure = 'ssl';
-		$phpmailer->Host = SMTP_HOST;
-		$phpmailer->Port = SMTP_PORT;
-		$phpmailer->SMTPAuth = true;
-		$phpmailer->Username = SMTP_USER;
-		$phpmailer->Password = SMTP_PASSWORD;
-	}
+        if( defined( 'SMTP_USER' ) && ! empty( SMTP_USER ) ) {
+            $phpmailer->SMTPAuth = true;
+            $phpmailer->Username = SMTP_USER;
+            $phpmailer->Password = defined( 'SMTP_PASSWORD' ) ?  (string) SMTP_PASSWORD : '';
+        }
+
+        $phpmailer->SMTPSecure = defined( 'SMTP_ENCRYPTION' ) ? (string) SMTP_ENCRYPTION : 'ssl';
+    }
 
 }
 
@@ -63,10 +55,10 @@ new Plugin;
 // remove plugin from update check response
 add_filter( 'site_transient_update_plugins', function( $value ) {
 
-	if( is_object( $value ) && isset( $value->response ) ) {
-		$plugin_slug = plugin_basename( __FILE__ );
-		unset( $value->response[ $plugin_slug ] );
-	}
+    if( is_object( $value ) && isset( $value->response ) ) {
+        $plugin_slug = plugin_basename( __FILE__ );
+        unset( $value->response[ $plugin_slug ] );
+    }
 
-	return $value;
+    return $value;
 } );
